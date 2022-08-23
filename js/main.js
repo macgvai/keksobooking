156e6664b73bfe6b3c8c4d8cd0d1277ee740d09b
macgvai <st.viktorr@gmail.com>
2022-08-23 19:31:06 +0300

    add main.js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomFloatingPointNumber = (min, max, decimalPlaces) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
+  return (min + Math.random() * (max - min)).toFixed(decimalPlaces);
+};

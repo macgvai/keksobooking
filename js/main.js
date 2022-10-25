849ced2ab4694ad00c9b7b4a2ac937fa45019324
macgvai <st.viktorr@gmail.com>
2022-10-25 19:43:10 +0300

    6.9. Помощь друга (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { renderCard, cardsArr, checkCard } from './card.js';
 import { onPriceOfType, onTimeIn,  onTimeOut } from './form.js';
 import  { map }  from './map.js';
+console.log(map);
 
 
 

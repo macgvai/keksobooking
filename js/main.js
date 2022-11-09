d196b8000da4597f5933fc7d8f3622962ddd4241
macgvai <st.viktorr@gmail.com>
2022-11-09 14:14:23 +0300

    7.15. Надо подкачаться 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import { renderCard, cardsArr, checkCard } from './card.js';
-import { onPriceOfType, onTimeIn,  onTimeOut, toInactiveForm, formValidation, setUserFormSubmit } from './form.js';
+import {  toInactiveForm, formValidation, setUserFormSubmit } from './form.js';
 import  { getMap, currentPosMarker }  from './map.js';
 const selectRooms = document.querySelector('#room_number');
 
@@ -7,17 +6,6 @@ const selectRooms = document.querySelector('#room_number');
 toInactiveForm();
 getMap();
 currentPosMarker();
-
-
-
-
-
-// cardsArr.forEach((element) => {
-//   renderCard(element.author, element.offer);
-//   console.log(element);});
-
-console.log(cardsArr);
-
 formValidation(selectRooms);
 setUserFormSubmit();
 

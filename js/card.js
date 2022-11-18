d4930088103aae7c1c09565fac0db69c54d820b7
macgvai <st.viktorr@gmail.com>
2022-11-18 20:33:24 +0300

    8.8. Перламутровые пуговицы (часть 2)

--- js/card.js
+++ js/card.js
@@ -46,9 +46,9 @@ const renderCard = function ({author, offer}) {
   newCard.querySelector('.popup__type').textContent = TYPES_OF_HOUSING[offer.type] || '';
   newCard.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей` || '';
   newCard.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}` || '';
-  newCard.querySelector('.popup__features').textContent = offer.features || '';
   newCard.querySelector('.popup__description').textContent = offer.description || '';
   newCard.querySelector('.popup__avatar').src = author.avatar || 'img/avatars/default.png';
+
 
   const cardFeatures = newCard.querySelector('.popup__features');
   cardFeatures.innerHTML = '';

7940ae1667250fe655d1e3e30ba965ce0eae3ef7
macgvai <st.viktorr@gmail.com>
2022-11-25 16:49:53 +0300

    9.3. Модель для сборки (необязательное задание)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const { SourceMap } = require('module');
 const path = require('path');
 
 module.exports = {
   entry: './source/js/main.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'build/js'),
     filename: 'main.bundle.js',
